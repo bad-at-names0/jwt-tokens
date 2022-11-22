8a85492dbb24a1d2eb09445f7cf4799d4325a6a9
bad-at-names0 <mayank234pathak@gmail.com>
2022-11-22 12:33:22 +0530

    Added the login and resgiter routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,31 @@
-const login = async (req, res) => {
-  res.send("Login route");
-};
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequest, Unauthorised, NotFound } = require("../errors");
 
 const register = async (req, res) => {
-  res.send("Register a user");
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequest(`Provide email and password`);
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new NotFound(`No user with email : ${email}`);
+  }
+
+  const isPasswordCorrect = await user.checkPassword(password);
+  if (!isPasswordCorrect) {
+    throw new Unauthorised(`Invalid password of the user`);
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    requried: [true, "Email is required"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide proper email",
+    ],
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minlength: 5,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: 5,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
+};
+
+UserSchema.methods.checkPassword = async function (inputPassword) {
+  const isPasswordCorrect = await bcrypt.compare(inputPassword, this.password);
+  return isPasswordCorrect;
+};
+
+module.exports = new mongoose.model("User", UserSchema);

3118c0d39a59db027cbd25bfb475b33feed4925f
bad-at-names0 <mayank234pathak@gmail.com>
2022-11-28 14:09:50 +0530

    Ports

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(errorHandlerMiddleware);
 app.use(notFoundpage);
 
 //connect to DB and start server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
